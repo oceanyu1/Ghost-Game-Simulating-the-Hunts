a5fc26eecc7354c89864d3d1889b027d07151fa5
oceanyu1 <81434548+oceanyu1@users.noreply.github.com>
2025-11-29 20:09:54 -0500

    Update main.c with initial house struct, initial ghost, and initial hunters

--- main.c
+++ main.c
@@ -22,13 +22,37 @@ int main() {
     
     srand(time(NULL)); 
 
+    struct House house;
+    house.hunter_count = 0;
+    house.room_count = 0;
+    house_populate_rooms(&house); 
+
+    house.case_file.collected = 0;
+    house.case_file.solved = false;
+    sem_init(&house.case_file.mutex, 0, 1);
+
     int ghost_start_idx = rand_int_threadsafe(1, house.room_count); 
-    if (ghost_start_idx == 0) ghost_start_idx = 1;
+    if (ghost_start_idx == 0) ghost_start_idx = 1; 
     
+    const enum GhostType* ghost_types;
+    int num_ghosts = get_all_ghost_types(&ghost_types);
+    enum GhostType g_type = ghost_types[rand_int_threadsafe(0, num_ghosts)];
+    
+    house.ghost = ghost_create(DEFAULT_GHOST_ID, g_type, &house.rooms[ghost_start_idx]);
+
     char name_buffer[MAX_HUNTER_NAME];
     printf("Please enter hunter names ('done' to cancel):\n");
     while (house.hunter_count < MAX_HUNTERS) {
-    
+        printf("Name: ");
+        scanf("%63s", name_buffer);
+        if (strcmp(name_buffer, "done") == 0) break;
+        
+        int h_id = house.hunter_count + 1; 
+
+        struct Hunter* h = hunter_create(name_buffer, h_id, house.starting_room, &house.case_file);
+        
+        house.hunters[house.hunter_count++] = h;
+        room_add_hunter(house.starting_room, h);
     }
 
     printf("\n--- Simulation Results ---\n");
